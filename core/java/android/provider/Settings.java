1c3286f64c3273929fece358067f49a9bb24df0c
maxwen <max.weninger@gmail.com>
2017-09-02 14:27:50 +0800

    Add WakelockBlocker [1/2]
    ported by faust93
    
    neobuddy89: Moved to global settings.
    
    Change-Id: I93fc64127c63a2d7986dff4f9c2a9941eaa56102
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13371,6 +13371,18 @@ public final class Settings {
                 "power_button_very_long_press";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+         /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Global settings that shouldn't be persisted.
          *
          * @hide
