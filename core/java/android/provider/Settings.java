b84fa6c34b918507e1ae158ad96e18da0f773489
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-04-08 20:34:51 +0530

    AppLock: Add option to instantly lock app on closing [1/2]
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>
    Change-Id: Ic527789a4f06be7458dc293f9ab4e2c25681f4bd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4995,11 +4995,10 @@ public final class Settings {
          */
         
         /**
-         * Indicates whether we should only show the app lock view when the device is woken up
-         * Or always.
+         * APP_LOCK_LOCK_AFTER key for user selected app lock timeout
          * @hide
          */
-        public static final String APP_LOCK_SHOW_ONLY_ON_WAKE = "app_lock_show_only_on_wake";
+        public static final String APP_LOCK_LOCK_AFTER = "applock_lock_after";
 
         /**
 
@@ -5138,7 +5137,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_FOURG);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
-            PRIVATE_SETTINGS.add(APP_LOCK_SHOW_ONLY_ON_WAKE);
+            PRIVATE_SETTINGS.add(APP_LOCK_LOCK_AFTER);
         }
 
         /**
