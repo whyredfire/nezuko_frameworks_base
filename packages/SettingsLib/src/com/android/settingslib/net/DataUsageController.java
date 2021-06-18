AICP/frameworks_base

[SQUASHED] SystemUI: QS Header data usage info [1/2]
Includes:

Author: Ali B <abittin@gmail.com>
Date:   Tue Jun 23 20:46:35 2020 +0300

    SystemUI: DataUsage: use proper connection icons

    Change-Id: I54e4eb3be07c64d7758f7010578f3d96b66b55c2

Author: AnkitGourav <Ankitgourav035@gmail.com>
Date:   Thu Jun 18 23:38:13 2020 +0300

    SystemUI: QSHeaderDataUsage: hide usage if device is disconnected

    - also clean-up DataUsageView

    Change-Id: I0b76d46c1ab2ec4b5c9d851ee74aa649bf60491d

Author: AnkitGourav <Ankitgourav035@gmail.com>
Date:   Thu Jun 18 16:09:04 2020 +0300

    SystemUI: QS Header data usage info: don't show used

    - also remove space between unit and value

    Change-Id: I2878b5831539f3ddc568add7a04d9dcb8e9f613b

Author: AnkitGourav <Ankitgourav035@gmail.com>
Date:   Thu May 28 06:24:17 2020 +0530

    SystemUI: QSHeaderDataUsage: show Wi-Fi usage when connected to Wi-Fi

    Change-Id: I7e3b04c4d20e2bcfeb659457a4f6639250fa55f3

Author: AnkitGourav <Ankitgourav035@gmail.com>
Date:   Thu May 28 06:21:10 2020 +0530

    AicpUtils: add method to check if device is connected to Wi-Fi

    Extracted from 085cb7424f5b8b451f2c92ef2d2fc81b5f103e0d

    Change-Id: I1d0fd663b123c0da66e976ac485f9411d5429756

Author: Alex Cruz <du.alexcruz@gmail.com>
Date:   Fri Dec 20 10:51:39 2019 -0500

    AicpUtils: Add method to check for both wifi and mobile connections

    Change-Id: If8f50e519778c03cb249a2ea8c7a7893c5b9f52f

Author: Ali B <abittin@gmail.com>
Date:   Thu Jun 11 14:59:35 2020 +0300

    DataUsageView: move it to QQS Status Icons area

    And properly size it to fit with the rest of
    the content there.
    Add some padding as well while making it
    overlayable in device trees

    Change-Id: Ic58df39f628e6baf9f407d0b94f4416d6185c38f

Author: AnkitGourav <Ankitgourav035@gmail.com>
Date:   Tue May 26 19:47:35 2020 +0530

    QSHeaderDataUsage: add icon and improve view

    - also improve system info layout
    - improve layout visibility

packages/SettingsLib/src/com/android/settingslib/net/DataUsageController.java
@@ -44,6 +44,7 @@ import android.util.Range;
 import com.android.internal.R;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.ArrayUtils;
+import com.android.internal.util.aicp.AicpUtils;
 
 import java.time.ZonedDateTime;
 import java.util.Iterator;
@@ -294,5 +295,43 @@ public class DataUsageController {
 
     public interface Callback {
         void onMobileDataEnabled(boolean enabled);
+    }
+
+    public DataUsageInfo getDailyDataUsageInfo() {
+        NetworkTemplate template = DataUsageUtils.getMobileTemplate(mContext, mSubscriptionId);
+        return getDailyDataUsageInfo(template);
+    }
+
+    public DataUsageInfo getDailyWifiDataUsageInfo() {
+        NetworkTemplate template = NetworkTemplate.buildTemplateWifiWildcard();
+        return getDailyDataUsageInfo(template);
+    }
+
+    public DataUsageInfo getDailyDataUsageInfo(NetworkTemplate template) {
+        final NetworkPolicy policy = findNetworkPolicy(template);
+        final long end = System.currentTimeMillis();
+        long start = end - AicpUtils.getTodayMillis();
+
+        final long totalBytes = getUsageLevel(template, start, end);
+        if (totalBytes < 0L) {
+            return warn("no entry data");
+        }
+        final DataUsageInfo usage = new DataUsageInfo();
+        usage.startDate = start;
+        usage.usageLevel = totalBytes;
+        usage.period = formatDateRange(start, end);
+        usage.cycleStart = start;
+        usage.cycleEnd = end;
+
+        if (policy != null) {
+            usage.limitLevel = policy.limitBytes > 0 ? policy.limitBytes : 0;
+            usage.warningLevel = policy.warningBytes > 0 ? policy.warningBytes : 0;
+        } else {
+            usage.warningLevel = getDefaultWarningLevel();
+        }
+        if (usage != null && mNetworkController != null) {
+            usage.carrier = mNetworkController.getMobileDataNetworkName();
+        }
+        return usage;
     }
 }

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -59,6 +59,7 @@ import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LifecycleRegistry;
 
+import com.android.internal.util.aicp.AicpUtils;
 import com.android.internal.util.aicp.FileUtils;
 import com.android.settingslib.Utils;
 import com.android.systemui.BatteryMeterView;
@@ -71,6 +72,7 @@ import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
 import com.android.systemui.qs.QSDetail.Callback;
 import com.android.systemui.qs.carrier.QSCarrierGroup;
 import com.android.systemui.statusbar.CommandQueue;
+import com.android.systemui.statusbar.info.DataUsageView;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.phone.StatusBarIconController.TintedIconManager;
 import com.android.systemui.statusbar.phone.StatusBarWindowView;
@@ -134,6 +136,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
     private int mRingerMode = AudioManager.RINGER_MODE_NORMAL;
     private AlarmManager.AlarmClockInfo mNextAlarm;
+
+    // Data Usage
+    private View mDataUsageLayout;
+    private ImageView mDataUsageImage;
+    private DataUsageView mDataUsageView;
 
     private ImageView mNextAlarmIcon;
     /** {@link TextView} containing the actual text indicating when the next alarm will go off. */
@@ -243,6 +250,13 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mClockView.setQsHeader();
         mDateView = findViewById(R.id.date);
         mDateView.setOnClickListener(this);
+        mDataUsageLayout = findViewById(R.id.daily_data_usage_layout);
+        mDataUsageImage = findViewById(R.id.daily_data_usage_icon);
+        mDataUsageView = findViewById(R.id.data_sim_usage);
+
+        updateDataUsageImage();
+        // Set the correct tint for the data usage icons so they contrast
+        mDataUsageImage.setImageTintList(ColorStateList.valueOf(fillColor));
 
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
@@ -470,6 +484,40 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         updateHeaderImage();
         updateResources();
         setBatteryPercentMode();
+        updateDataUsageView();
+        updateDataUsageImage();
+    }
+
+    private void updateDataUsageView() {
+        if (mDataUsageView.isDataUsageEnabled() != 0) {
+            if (AicpUtils.isConnected(mContext)) {
+                DataUsageView.updateUsage();
+                mDataUsageLayout.setVisibility(View.VISIBLE);
+                mDataUsageImage.setVisibility(View.VISIBLE);
+                mDataUsageView.setVisibility(View.VISIBLE);
+            } else {
+                mDataUsageView.setVisibility(View.GONE);
+                mDataUsageImage.setVisibility(View.GONE);
+                mDataUsageLayout.setVisibility(View.GONE);
+            }
+        } else {
+            mDataUsageView.setVisibility(View.GONE);
+            mDataUsageImage.setVisibility(View.GONE);
+            mDataUsageLayout.setVisibility(View.GONE);
+        }
+    }
+
+    public void updateDataUsageImage() {
+        if (mDataUsageView.isDataUsageEnabled() == 0) {
+            mDataUsageImage.setVisibility(View.GONE);
+        } else {
+            if (AicpUtils.isWiFiConnected(mContext)) {
+                mDataUsageImage.setImageDrawable(mContext.getDrawable(R.drawable.ic_data_usage_wifi));
+            } else {
+                mDataUsageImage.setImageDrawable(mContext.getDrawable(R.drawable.ic_data_usage_cellular));
+            }
+            mDataUsageImage.setVisibility(View.VISIBLE);
+        }
     }
 
     private void updateStatusIconAlphaAnimator() {
@@ -505,6 +553,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateSystemInfoText();
         updateEverything();
+        updateDataUsageView();
     }
 
     /**

packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
@@ -1,0 +1,81 @@
+package com.android.systemui.statusbar.info;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.provider.Settings;
+import android.telephony.SubscriptionManager;
+import android.text.BidiFormatter;
+import android.text.format.Formatter;
+import android.text.format.Formatter.BytesResult;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.android.internal.util.aicp.AicpUtils;
+import com.android.settingslib.net.DataUsageController;
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.StatusBar;
+import com.android.systemui.statusbar.policy.NetworkController;
+
+public class DataUsageView extends TextView {
+
+    private Context mContext;
+    private NetworkController mNetworkController;
+    private static boolean shouldUpdateData;
+    private String formatedinfo;
+
+    public DataUsageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mContext = context;
+        mNetworkController = Dependency.get(NetworkController.class);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if ((isDataUsageEnabled() == 0) && this.getText().toString() != "") {
+            setText("");
+        } else if (isDataUsageEnabled() != 0 && shouldUpdateData) {
+            shouldUpdateData = false;
+            updateUsageData();
+            setText(formatedinfo);
+        }
+        updateDataUsageImage();
+    }
+
+    private void updateUsageData() {
+        DataUsageController mobileDataController = new DataUsageController(mContext);
+        mobileDataController.setSubscriptionId(
+            SubscriptionManager.getDefaultDataSubscriptionId());
+        final DataUsageController.DataUsageInfo info = isDataUsageEnabled() == 1 ?
+                (AicpUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getDailyWifiDataUsageInfo()
+                        : mobileDataController.getDailyDataUsageInfo())
+                : (AicpUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getWifiDataUsageInfo()
+                        : mobileDataController.getDataUsageInfo());
+
+        formatedinfo = formatDataUsage(info.usageLevel);
+    }
+
+    public int isDataUsageEnabled() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QS_DATAUSAGE, 0);
+    }
+
+    public static void updateUsage() {
+        shouldUpdateData = true;
+    }
+
+    private void updateDataUsageImage() {
+        StatusBar statusBar = Dependency.get(StatusBar.class);
+        statusBar.updateDataUsageImage();
+    }
+
+    private String formatDataUsage(long byteValue) {
+        final BytesResult res = Formatter.formatBytes(mContext.getResources(), byteValue,
+                Formatter.FLAG_IEC_UNITS);
+        return BidiFormatter.getInstance().unicodeWrap(res.value + res.units);
+    }
+}

whyredfire/nezuko_frameworks_base

Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SettingsLib/src/com/android/settingslib/net/DataUsageController.java
@@ -44,6 +44,7 @@ import android.util.Range;
 import com.android.internal.R;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.ArrayUtils;
+import com.android.internal.util.nezuko.NezukoUtils;
 
 import java.time.ZonedDateTime;
 import java.util.Iterator;
@@ -294,5 +295,43 @@ public class DataUsageController {
 
     public interface Callback {
         void onMobileDataEnabled(boolean enabled);
+    }
+
+    public DataUsageInfo getDailyDataUsageInfo() {
+        NetworkTemplate template = DataUsageUtils.getMobileTemplate(mContext, mSubscriptionId);
+        return getDailyDataUsageInfo(template);
+    }
+
+    public DataUsageInfo getDailyWifiDataUsageInfo() {
+        NetworkTemplate template = NetworkTemplate.buildTemplateWifiWildcard();
+        return getDailyDataUsageInfo(template);
+    }
+
+    public DataUsageInfo getDailyDataUsageInfo(NetworkTemplate template) {
+        final NetworkPolicy policy = findNetworkPolicy(template);
+        final long end = System.currentTimeMillis();
+        long start = end - NezukoUtils.getTodayMillis();
+
+        final long totalBytes = getUsageLevel(template, start, end);
+        if (totalBytes < 0L) {
+            return warn("no entry data");
+        }
+        final DataUsageInfo usage = new DataUsageInfo();
+        usage.startDate = start;
+        usage.usageLevel = totalBytes;
+        usage.period = formatDateRange(start, end);
+        usage.cycleStart = start;
+        usage.cycleEnd = end;
+
+        if (policy != null) {
+            usage.limitLevel = policy.limitBytes > 0 ? policy.limitBytes : 0;
+            usage.warningLevel = policy.warningBytes > 0 ? policy.warningBytes : 0;
+        } else {
+            usage.warningLevel = getDefaultWarningLevel();
+        }
+        if (usage != null && mNetworkController != null) {
+            usage.carrier = mNetworkController.getMobileDataNetworkName();
+        }
+        return usage;
     }
 }

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -59,6 +59,7 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LifecycleRegistry;
 
 import com.android.internal.logging.UiEventLogger;
+import com.android.internal.util.nezuko.NezukoUtils;
 import com.android.settingslib.Utils;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.DualToneHandler;
@@ -75,6 +76,7 @@ import com.android.systemui.privacy.PrivacyItemController;
 import com.android.systemui.qs.QSDetail.Callback;
 import com.android.systemui.qs.carrier.QSCarrierGroup;
 import com.android.systemui.statusbar.CommandQueue;
+import com.android.systemui.statusbar.info.DataUsageView;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.phone.StatusBarIconController.TintedIconManager;
 import com.android.systemui.statusbar.phone.StatusBarWindowView;
@@ -140,6 +142,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
     private int mRingerMode = AudioManager.RINGER_MODE_NORMAL;
     private AlarmManager.AlarmClockInfo mNextAlarm;
+
+    // Data Usage
+    private View mDataUsageLayout;
+    private ImageView mDataUsageImage;
+    private DataUsageView mDataUsageView;
 
     private ImageView mNextAlarmIcon;
     /** {@link TextView} containing the actual text indicating when the next alarm will go off. */
@@ -280,9 +287,18 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
+
         mDateView = findViewById(R.id.date);
-        mSpace = findViewById(R.id.space);
         mDateView.setOnClickListener(this);
+
+        mDataUsageLayout = findViewById(R.id.daily_data_usage_layout);
+        mDataUsageImage = findViewById(R.id.daily_data_usage_icon);
+        mDataUsageView = findViewById(R.id.data_sim_usage);
+
+        updateDataUsageImage();
+        // Set the correct tint for the data usage icons so they contrast
+        mDataUsageImage.setImageTintList(ColorStateList.valueOf(fillColor));
+        mSpace = findViewById(R.id.space);
 
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.battery);
@@ -293,6 +309,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mBatteryRemainingIcon.setPercentShowMode(BatteryMeterView.MODE_ESTIMATE);
         mRingerModeTextView.setSelected(true);
         mNextAlarmTextView.setSelected(true);
+        updateSettings();
 
         mAllIndicatorsEnabled = mPrivacyItemController.getAllIndicatorsAvailable();
         mMicCameraIndicatorsEnabled = mPrivacyItemController.getMicCameraAvailable();
@@ -462,6 +479,45 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         updatePrivacyChipAlphaAnimator();
     }
 
+
+    private void updateSettings() {
+        updateDataUsageView();
+        updateDataUsageImage();
+     }
+
+
+    private void updateDataUsageView() {
+        if (mDataUsageView.isDataUsageEnabled() != 0) {
+            if (NezukoUtils.isConnected(mContext)) {
+                DataUsageView.updateUsage();
+                mDataUsageLayout.setVisibility(View.VISIBLE);
+                mDataUsageImage.setVisibility(View.VISIBLE);
+                mDataUsageView.setVisibility(View.VISIBLE);
+            } else {
+                mDataUsageView.setVisibility(View.GONE);
+                mDataUsageImage.setVisibility(View.GONE);
+                mDataUsageLayout.setVisibility(View.GONE);
+            }
+        } else {
+            mDataUsageView.setVisibility(View.GONE);
+            mDataUsageImage.setVisibility(View.GONE);
+            mDataUsageLayout.setVisibility(View.GONE);
+        }
+    }
+
+    public void updateDataUsageImage() {
+        if (mDataUsageView.isDataUsageEnabled() == 0) {
+            mDataUsageImage.setVisibility(View.GONE);
+        } else {
+            if (NezukoUtils.isWiFiConnected(mContext)) {
+                mDataUsageImage.setImageDrawable(mContext.getDrawable(R.drawable.ic_data_usage_wifi));
+            } else {
+                mDataUsageImage.setImageDrawable(mContext.getDrawable(R.drawable.ic_data_usage_cellular));
+            }
+            mDataUsageImage.setVisibility(View.VISIBLE);
+        }
+    }
+
     private void updateStatusIconAlphaAnimator() {
         mStatusIconsAlphaAnimator = new TouchAnimator.Builder()
                 .addFloat(mQuickQsStatusIcons, "alpha", 1, 0, 0)
@@ -485,6 +541,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mExpanded = expanded;
         mHeaderQsPanel.setExpanded(expanded);
         updateEverything();
+        updateDataUsageView();
     }
 
     /**

packages/SystemUI/src/com/android/systemui/statusbar/info/DataUsageView.java
@@ -1,0 +1,81 @@
+package com.android.systemui.statusbar.info;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.provider.Settings;
+import android.telephony.SubscriptionManager;
+import android.text.BidiFormatter;
+import android.text.format.Formatter;
+import android.text.format.Formatter.BytesResult;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.android.internal.util.nezuko.NezukoUtils;
+import com.android.settingslib.net.DataUsageController;
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.StatusBar;
+import com.android.systemui.statusbar.policy.NetworkController;
+
+public class DataUsageView extends TextView {
+
+    private Context mContext;
+    private NetworkController mNetworkController;
+    private static boolean shouldUpdateData;
+    private String formatedinfo;
+
+    public DataUsageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mContext = context;
+        mNetworkController = Dependency.get(NetworkController.class);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if ((isDataUsageEnabled() == 0) && this.getText().toString() != "") {
+            setText("");
+        } else if (isDataUsageEnabled() != 0 && shouldUpdateData) {
+            shouldUpdateData = false;
+            updateUsageData();
+            setText(formatedinfo);
+        }
+        updateDataUsageImage();
+    }
+
+    private void updateUsageData() {
+        DataUsageController mobileDataController = new DataUsageController(mContext);
+        mobileDataController.setSubscriptionId(
+            SubscriptionManager.getDefaultDataSubscriptionId());
+        final DataUsageController.DataUsageInfo info = isDataUsageEnabled() == 1 ?
+                (NezukoUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getDailyWifiDataUsageInfo()
+                        : mobileDataController.getDailyDataUsageInfo())
+                : (NezukoUtils.isWiFiConnected(mContext) ?
+                        mobileDataController.getWifiDataUsageInfo()
+                        : mobileDataController.getDataUsageInfo());
+
+        formatedinfo = formatDataUsage(info.usageLevel);
+    }
+
+    public int isDataUsageEnabled() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QS_DATAUSAGE, 0);
+    }
+
+    public static void updateUsage() {
+        shouldUpdateData = true;
+    }
+
+    private void updateDataUsageImage() {
+        StatusBar statusBar = Dependency.get(StatusBar.class);
+        statusBar.updateDataUsageImage();
+    }
+
+    private String formatDataUsage(long byteValue) {
+        final BytesResult res = Formatter.formatBytes(mContext.getResources(), byteValue,
+                Formatter.FLAG_IEC_UNITS);
+        return BidiFormatter.getInstance().unicodeWrap(res.value + res.units);
+    }
+}